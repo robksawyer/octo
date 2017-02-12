d7009b0d82cdc7e443e475bbed3faf23e107a244
Rob Sawyer <robksawyer@gmail.com>
2017-02-12 02:59:02 -0800

    Hacked a very crappy implementation of part search via a category

--- index.js
+++ index.js
@@ -21,12 +21,22 @@ function encodeFilters(filters) {
 	return flatten(Object.keys(filters).map(function(k) {
 		var v = Array.prototype.concat.apply([], filters[k]);
 		return v.map(function(vi) {
-			if (vi instanceof Object)
+			if (vi instanceof Object) {
 				return Object.keys(vi).map(function(ki) {
 					return k + '[' + ki + ']=' + querystring.escape(vi[ki]); // e.g. slice[field1]=1:5
 				});
-			else
+			} else {
 				return k + '[]=' + querystring.escape(vi); // e.g. show[]=foo
+			}
+		});
+	}));
+}
+
+function encodeCatFilters(filters) {
+	return flatten(Object.keys(filters).map(function(k) {
+		var v = Array.prototype.concat.apply([], filters[k]);
+		return v.map(function(vi) {
+			return 'filters=["'+ k + '"[' + vi + ']]';
 		});
 	}));
 }
@@ -35,19 +45,51 @@ var OctoNode = function(apikey, apipath) {
 	var self = this;
 
 	var send = function(path, params, filters, cb) {
-		if (typeof filters === 'function')
+		if (typeof filters === 'function') {
 			cb = filters; // skip filters
-		else if (filters)
+		} else if (filters) {
 			params = params.concat(encodeFilters(filters));
+		}
 		var opt = {
-			headers: { Accept: 'application/json' },
-			url: url.format({
+			headers: {
+				Accept: 'application/json' },
+				url: url.format({
 				protocol: 'https',
 				host: 'octopart.com',
 				pathname: apipath + path,
 				search: params.join('&') + '&apikey=' + apikey
 			})
 		};
+		return request.get(opt, cb ? function(err, res, body) {
+			if (err)
+				cb(err);
+			else if (res.statusCode != 200)
+				cb(new Error(JSON.parse(body).message));
+			else
+				cb(null, JSON.parse(body));
+		} : null);
+	};
+
+	var sendVariation = function(path, params, filters, cb) {
+		if (typeof filters === 'function') {
+			cb = filters; // skip filters
+		} else if (filters) {
+			console.log(filters);
+			console.log('==============');
+			params = params.concat(encodeCatFilters(filters));
+			console.log(params);
+		}
+		var opt = {
+			headers: {
+				Accept: 'application/json' },
+				url: url.format({
+				protocol: 'https',
+				host: 'octopart.com',
+				pathname: apipath + path,
+				search: params.join('&') + '&apikey=' + apikey
+			})
+		};
+		console.log(opt);
 		return request.get(opt, cb ? function(err, res, body) {
 			if (err)
 				cb(err);
@@ -79,6 +121,24 @@ var OctoNode = function(apikey, apipath) {
 			return send(name + '/search', params, filters, cb);
 		};
 	});
+
+	// Handles searching for parts via category id(s)
+	// ex. partsByCategory([3394, 445], {limit: 15}, cb)
+	self.partsByCategory = function(uids, params, cb){
+		// var params = [].concat(args).map(function(key) {
+		// 	return querystring.stringify(key);
+		// });
+		var filters = {};
+		if (Array.isArray(uids)) {
+			filters.category_id = [].concat(uids).map(function(uid) {
+				return uid;
+			});
+			console.log(filters);
+		} else {
+			filters = { category_id: uid };
+		}
+		return sendVariation('parts/search', params, filters, cb);
+	}
 
 	// args = { queries: [{...}, {...}], exact_only: true }
 	// filters = response filters
